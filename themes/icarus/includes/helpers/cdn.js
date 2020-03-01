8d8e33bb283f0db28c04d137843e7b38caaf585c
wangchengkai <me@chengkai.wang>
2020-03-01 14:50:58 +0800

    fix layout

--- themes/icarus/includes/helpers/cdn.js
+++ themes/icarus/includes/helpers/cdn.js
@@ -17,7 +17,7 @@ const font_providers = {
 };
 
 const icon_providers = {
-    fontawesome: 'https://use.fontawesome.com/releases/v5.4.1/css/all.css'
+    fontawesome: '/css/font-awesome-4.7.0/css/font-awesome.min.css'
 };
 
 module.exports = function (hexo) {
